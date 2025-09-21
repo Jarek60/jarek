a42a62d216c3b57f07932b3fe5aa4dd20347a76d
Jarek <jarek06@berkeley.edu>
2025-09-21 02:28:54 -0700

    done with hw2

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -1,6 +1,8 @@
 #homework1.py
  # --- Variables and Datatypes ---
 
+# --- Variables and Data Types ---
+
 a = 10
 print(a)
 print(type(a)) # a is an integer, a whole number with no decimals
@@ -61,25 +63,164 @@ print(type(m)) # m is a float, 1*10^4 so scientific notation
 5. A range, an immutable seuence of numbers.
 """
 
+# --- Booleans ---
+
 print(10 > 9) # True, 10 is greater than 9
 print(10 == 9) # False, 9 isnt equal to 10
 print(10 <= 9) # False, 10 is not less than or equal to 9
-bool("abc")
-bool(123)
-bool(["apple","cherry", "banana"])
-bool(True)
-bool(False)
-bool(0)
-bool("")
-bool(" ")
-bool(())
-bool([])
-bool({})
-bool(True and False)
-bool(True and True)
-bool(False and False)
-bool(True or False)
-bool(True or True)
-bool(False or False)
-bool(not(False))
-bool(not(True))
+print(bool("abc")) #True,
+print(bool(123)) #True
+print(bool(["apple","cherry", "banana"])) #True,
+print(bool(True)) #True,
+print(bool(False)) #False
+print(bool(0)) #False
+print(bool("")) #False
+print(bool(" ")) #True
+print(bool(())) #False
+print(bool([])) #False
+print(bool({})) #False
+print(bool(True and False)) #False
+print(bool(True and True)) #True
+print(bool(False and False)) #False
+print(bool(True or False)) #True
+print(bool(True or True)) #True
+print(bool(False or False)) #False
+print(bool(not(False))) #True
+print(bool(not(True))) #False
+
+"""
+1. The lines with nothing in them, not even a space were false and most of the lines with soemthing in them there was a true unless the word false was major in that line.
+2. The Tre and false line suprised me as it was false.
+3. bool(56) should reutrn true since as seen above the lines with single numbers or strings in them return true.
+4. bool('') should return false as the lines with nothing in the parantheses reutrns false.
+"""
+
+# --- Operators ---
+
+    # --- Arithmetic Operators ---
+
+print(10 + 5) #15, performs addition
+print(10 - 5) #5, performs subtraction
+print(2 * 4) #8, performs multiplication
+print(6 / 3) #2, performs division
+print(5 % 2) #1, returns the remainder of division
+print(3 ** 2) #9, raises to the power of the second number
+print(15 // 2) #7, performs division and rounds to the nearest integer
+
+    # --- Comparison Operators ---
+
+print(5 == 2) #False, compares for euqality
+print(10 != 10) #False, compares for non equality
+print(2 < 5) #True, compares for less than
+print(12 > 5) #True, compares for greater than
+print(5 <= 6) #True, compares for less than or equal to
+print(1 >= 10) #False, compares for greater than or equal to
+
+    # --- Assignment Operators ---
+
+x = 5
+x += 5 #10, performs addition and the sum becomes x
+print(x)
+x -= 4 #6, performs subtraction to previous x and difference becomes x
+print(x)
+x *= 3 #18, performs multiplication to previous x and multiple becomes x
+print(x)
+
+    # --- Logical Operators ---
+'''
+The operator and makes two arguments have to be true or occur at the same time. (x == x and x > x) should be false.
+The operator or makes one of two arguments have to be true. (x < x or x > x) should be false.
+The operator not makes returns the opposite of the result from the function given. (not x == x) would be false.
+
+1. The difference between / and // is that // rounds the quotient to the nearest integer.
+2. The difference between % and // is that // rounds the quotient and % gives the remainder of the of the quotient.
+3. You would use % to calculate the remainder. 7 % 2 gives 1 as the remainder.
+4. Assignment operators rquire a variable and when the operator is resolved they assign x to the new value from the expression.
+'''
+# --- String ---
+
+my_string = "hello"
+print(my_string) #prints: hello
+print(my_string[0]) #prints: h
+print(my_string[1]) #prints: e
+print(my_string[2]) #prints: l
+print(my_string[3]) #prints: l
+print(my_string[4]) #prints: o
+print(my_string[-1]) #prints: o
+print(my_string[1:3]) #prints: el
+print(my_string[0:5:2]) #prints: hlo
+print(len(my_string)) #prints: 5
+print(my_string + "goodbye") #prints: hellogoodbye
+print(my_string * 7) #prints: hellohellohellohellohellohellohello
+
+'''
+1. slicing takes subsections of the things in a list or string. In this case it's the letters in hello.
+2. name = "Oski" print("Hello, my name is", name) results in Hello, my name is Oski being printed.
+3. name = "Oski" print(f"Hello, my name is {name}") also results in Hello, my name is Oski being printed.
+4. The difference bwteen the last two strings is that the f-string is a more concise and efficient way of putting expression in your strings using the {} brackets.
+'''
+
+# --- Terminal Commands ---
+
+# cd
+# Changes directories. Use it to move from one folder to another
+# Example: cd Desktop
+
+# ls
+# List. Use it to list the contents of a directory.
+# Example: ls
+
+# ls -a
+# List all. Use it to list all files including the hidden files.
+# Example: ls -a
+
+# mkdir
+# Make directory. Use it to make a folder where you are in the terminal.
+# Example: mkdir Book.py
+
+# cat
+# Use it to print the contents of your file in the terminal without running the code.
+# Example: cat Book.py
+
+# pwd
+# Print working directory. Use it to show where in the terminal you are going from the main directory to the file or directory you are in.
+# Example: pwd
+
+# cd ..
+# change directory ... Use it to go to the parent directory of the file/directory you are in.
+# Example: cd ..
+
+# cd .
+# Change directory .. Use it to stay in the directory you are in.
+# Example: cd .
+
+# cd ~
+# Change directory ~. Use it to go straight to the home directory.
+# Example: cd ~
+
+# cp
+# Copy. Use it to copy files.
+# Example: cp Book.py
+
+# mv
+# Move files. Use it to move files and directories into other directories.
+# Example: mv Book.py Jarek/
+
+# rm
+# Remove. Use it to remove and delete files.
+# Examples: rm Book.py
+
+# clear
+# Clear. Use it to clear the terminal screen.
+# clear_screen()
+
+# grep
+# Use it to print lines in your code with specific patterns or words.
+# Example: grep Coding Book.py
+
+'''
+1. Touch is used to make a new file in the directory you're in, touch book.py. Nano is used to modify code lines in the designated file from the terminal, nano book.py. Python3 is used to run your code in the terminal, python3 book.py.
+2. The difference between ls and ls -a is that ls -a shows all files including hidden files.
+3. Hidden files are not shown by ls command and sometimes begin with a dot.
+4. -i is a flag for grep tp be case insenstive when searching, grep -i CODE Book.py. -R for ls used for listing all files in subdirectories, ls -R. -r for rm used for deleting a folder and all its contents, rm -r.
+'''
---
+++ homework2/homework2.py
@@ -0,0 +1,66 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+#    Git is a set of command tools to help keep track, make, and coooperate locally on changes of projects. Github is the website platform that hosts the git repositories and allows for sharing and extended functionality of Git. Git bash is an app for Windows that provides a interface for useing Git as intended.
+
+# 2) What’s the difference between the terminal and the command line?
+#    The command line accessible in the terminal, the terminal allows you to access all files and directories in your computer which from there you can go to the command line of the individual files where you can modify the code and enter commands.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+#    Powershell is native to windows while git bash emulates on windows. Powershell is made for windows administration and automation, while git bash is for git operations and unix-like environment on windows.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+#    Python is the progamming code itself and its used to run python scripts and use libraries, Conda manages packages and environments, it works for python and other packages. It can also install versions of python, anaconda is a python distribution with python and conda pre-installed. Its designed for scientific computing and includes several other packages.
+
+# 5) What is VS Code? 
+#    It's a code editor that supports python, has extensions like git, and works for windows and mac. It's good for quick scripts and large projects.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+#    Notebook is an online environment to write and run python, it lets you make notebooks with cells and inlines making it helpful for data science and scientific computing. Lab is the newer version for Jupyter, it lets you work on multiple notebooks and drag and drop things making it more flexible than notebook which came first.
+
+# 7) What does ~/ mean?
+#    ~/ means you are in your home or main directory that holds everything in your computer.
+
+# 8) What’s the difference between an absolute path and a relative path?
+#    An absolute path gives the name of every folder starting from ~/ up to the folder you are in showing the path you took to get there. Relative path only shows you the parent and child directories you are in.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+#    Starting from my jarek repo the absolute path is jarekescobar/python_decal_fa25/course_assignments/homework2. The relative path is ../python_decal_fa25/course_assignments/homework2.
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+#     The command cd .. lets you move there.
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+#     rm ./ would delete my current directory and file.
+
+# 12) What do the following commands do?
+# git add - allows you to add the files and changes you want to save.
+# git commit - saves your changes and files that were added onto your local repository.
+# git push - saves your local changes onto githubs online repository.
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+#     git add . stages all changes in your current directory and in its subdirectories while git add is only for the specific file mentioned.
+
+# 14) What do "git status" and "git log -1" do?
+#     git status lets you see which files are modified and git log -1 shows the most recent commit history made.
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+#     The difference is that the clone repository doesnt belong to you and it creates a copy of all the contents in it. Pulling from it updates your local repo that was cloned, with the latest remote changes.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+#     My most frustrating error was making changes on github then not pulling the changes onto the terminal and vs code before making changes in vscode on the same file so when i tried to save there were errors because it had to push and pull the changes at the same time and didn't work properly. I stopped the sync and deleted the changes on vscode and then properly pulled the changes from github to have the most up to date version.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+#     How can we graph data points on vscode? I was a little confused on booleans but it cleared up.
+
+# 18) Tell me a fun fact!
+#     Python is named after "Monty Python's Flying Circus."
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(9754 % 37) # It gives the remainder of the function 9754/37
